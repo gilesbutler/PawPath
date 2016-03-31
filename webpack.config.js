0c09fa6cc67e9a4043c61c92eb7af4db95cb2261
Giles Butler <giles@mixvisor.com>
2016-04-01 06:51:42 +1100

    Add react-css-modules

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,19 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'client')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'client')
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+            'style?sourceMap',
+            'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+        ]
+      }
+    ]
   }
 };

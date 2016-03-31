b1c08641a2409a214e986a24ec9d969731a6ac56
Giles Butler <giles@mixvisor.com>
2016-03-31 18:48:32 +1100

    Add Reqwest package Search Aus Post API

--- client/app/lookup/LookupContainer.js
+++ client/app/lookup/LookupContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import reqwest from 'reqwest';
 import Lookup from './LookupComponent';
 import Dispatcher from '../../dispatcher/dispatcher';
 import LookupConstants from '../../constants/LookupConstants';
@@ -25,6 +26,20 @@ export default class LookupContainer extends Component {
   }
 
   searchAusPostAPI(query) {
-    console.log(query);
+    reqwest({
+      url: 'https://test.npe.auspost.com.au/api/postcode/search.json?q=' + query,
+      headers: {
+        'auth-key': '28744ed5982391881611cca6cf5c240'
+      },
+      crossOrigin: true,
+      method: 'get',
+      error: function (err) {
+        console.log('REQWEST ERROR:');
+        console.log(err);
+      },
+      success: function (resp) {
+        console.log(resp);
+      }
+    });
   }
 }
